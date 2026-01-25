3c89e09080aa89c7df92b1fb76f215009ccbd2a0
codezen-dev <lizhenweijie@gmail.com>
2026-01-25 16:12:02 +0800

    修改接口路径

--- src/main/java/com/fly/forgotyet/controller/EventController.java
+++ src/main/java/com/fly/forgotyet/controller/EventController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/fy-api/api/events")
+@RequestMapping("/fy-api/api/event")
 @RequiredArgsConstructor
 public class EventController {
 
